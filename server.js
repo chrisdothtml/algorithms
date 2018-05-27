1d3298dce168615657e3d26ebd0726ca9a9fdde9
Chris Deacy <chrisdothtml@gmail.com>
2018-05-26 23:13:43 -0500

    replacing custom server with http-server command

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const PORT = 1337
-const server = express()
-
-server.use(express.static('.'))
-server.listen(PORT, () => {
-  console.log(`server running at http://localhost:${PORT}`)
-})
